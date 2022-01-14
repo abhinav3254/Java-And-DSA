ea1c511411c41b956411fef59f465f8615d31d1c
abhinav3254 <abhinav3254@gmail.com>
2022-01-14 23:33:38 +0530

    Day-1

--- src/Java/ExceptionHandling/ExceptionHandlingOne.java
+++ src/Java/ExceptionHandling/ExceptionHandlingOne.java
@@ -1,7 +1,7 @@
 package Java.ExceptionHandling;
 
 public class ExceptionHandlingOne {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         int a = 10;
         int b = 0;
         try{
@@ -10,6 +10,20 @@ public class ExceptionHandlingOne {
             System.out.println(e.getMessage());
         } finally {
             System.out.println("This block will get Executed");
+        }
+
+        try{
+            System.out.println(sum(a,b));
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    static int sum(int a,int b) throws Exception {
+        if(b==0){
+            throw new Exception("Denominator must not be Zero");
+        } else {
+            return a/b;
         }
     }
 }
