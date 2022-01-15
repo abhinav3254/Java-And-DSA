d6304a6634079b9d610a60da9e401a29904a0e3e
abhinav3254 <abhinav3254@gmail.com>
2022-01-15 13:48:10 +0530

    Enums

---
+++ src/Java/collectionframework/LL.java
@@ -0,0 +1,17 @@
+package Java.collectionframework;
+
+import java.util.LinkedList;
+
+public class LL {
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+        linkedList.add(12);
+        linkedList.add(24);
+        linkedList.add(24);
+        linkedList.add(24);
+        linkedList.add(24);
+        linkedList.add(24);
+
+        System.out.println(linkedList);
+    }
+}
---
+++ src/Java/enumexamples/Basic.java
@@ -0,0 +1,21 @@
+package Java.enumexamples;
+
+public class Basic {
+
+    enum Week {
+        Monday,Tuesday,Wednesday,Thrusday,Friday,Staurday,Sunday
+    }
+
+    public static void main(String[] args) {
+
+        Week week;
+        week = Week.Monday;
+        System.out.println(week);
+
+        for (Week day:
+                Week.values()
+             ) {
+            System.out.println(day);
+        }
+    }
+}
