c1800f9ad944740ad814dbebdfda515d400240d0
abhinav3254 <abhinav3254@gmail.com>
2022-01-16 16:18:49 +0530

    Doubly Linked List Practise

---
+++ src/DSA/doublylinkedlist/self/DLL.java
@@ -0,0 +1,38 @@
+package DSA.doublylinkedlist.self;
+
+public class DLL {
+    private Node head;
+    private Node tail;
+    private int size;
+
+
+    public void addFirst (int data) {
+        Node node = new Node(data);
+        node.next = head;
+        node.prev = null;
+        if(head!=null){
+            head.prev = node;
+        }
+        head = node;
+    }
+
+    public void display(){
+        Node temp = head;
+        System.out.print("Null");
+        while (temp!=null){
+            System.out.print(" <-"+temp.data+"->");
+            temp = temp.next;
+        }
+        System.out.println("Null");
+    }
+
+    class Node {
+        private int data;
+        private Node next;
+        private Node prev;
+
+        public Node(int data) {
+            this.data = data;
+        }
+    }
+}
---
+++ src/DSA/doublylinkedlist/self/Main.java
@@ -0,0 +1,14 @@
+package DSA.doublylinkedlist.self;
+
+public class Main {
+    public static void main(String[] args) {
+        DLL dll = new DLL();
+        dll.addFirst(1);
+        dll.addFirst(2);
+        dll.addFirst(3);
+        dll.addFirst(4);
+        dll.addFirst(5);
+
+        dll.display();
+    }
+}
