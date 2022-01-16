11520a40169b6d08d883ed3399659f8f0900c4da
abhinav3254 <abhinav3254@gmail.com>
2022-01-16 12:21:11 +0530

    llp class and Main1 class Added for practise handled error also

---
+++ src/DSA/singlylinkedlist/LLP.java
@@ -0,0 +1,81 @@
+package DSA.singlylinkedlist;
+
+public class LLP {
+    private Node head;
+    private Node tail;
+    private int size=0;
+
+    public void insertFirst (int data) {
+        Node node = new Node(data);
+
+        node.next = head;
+        head = node;
+
+        if(tail == null) {
+            tail = head;
+        }
+        size++;
+    }
+
+    public void insertLast (int data) {
+        if(tail == null){
+            System.out.println("Insert Data First");
+            return;
+        }
+        Node node = new Node(data);
+        tail.next = node;
+        tail = node;
+        size++;
+    }
+
+    public void printData () {
+        Node temp = head;
+        while (temp!=null) {
+            System.out.print(temp.data+"->");
+            temp = temp.next;
+        }
+        System.out.println("End");
+    }
+
+
+    public void insertAtIndex (int data,int index) {
+        try {
+            if (index == 0) {
+                insertFirst(data);
+                return;
+            }
+            if (index == size) {
+                insertLast(data);
+                return;
+            }
+            if(index>size || index<0) {
+                throw new Exception();
+            }
+            Node node = new Node(data);
+            Node temp = head;
+            Node temp1 = head.next;
+            for (int i = 1; i < index-1; i++) {
+                temp = temp.next;
+                temp1 = temp1.next;
+            }
+            temp.next = node;
+            node.next = temp1;
+        } catch (Exception e){
+            System.out.println("Enter Valid Index");
+        }
+    }
+
+    class Node {
+        private int data;
+        private Node next;
+
+        public Node(int data) {
+            this.data = data;
+        }
+
+        public Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+}
---
+++ src/DSA/singlylinkedlist/Main1.java
@@ -0,0 +1,38 @@
+package DSA.singlylinkedlist;
+
+public class Main1 {
+    public static void main(String[] args) {
+        LLP llp = new LLP();
+        llp.insertFirst(1);
+        llp.insertFirst(2);
+        llp.insertFirst(3);
+        llp.insertFirst(4);
+        llp.insertFirst(5);
+
+        llp.printData();
+
+//        llp.insertLast(100);
+//        llp.insertLast(99);
+//        llp.insertLast(98);
+//        llp.insertLast(97);
+//        llp.insertLast(96);
+//        llp.insertLast(95);
+//
+//        llp.printData();
+
+        llp.insertAtIndex(25,5);
+        try {
+            System.out.print("Inserting Data wait a while");
+            for (int i = 1; i <4 ; i++) {
+                System.out.print(".");
+                Thread.sleep(1000);
+            }
+        } catch (Exception e){
+            System.out.println(e.getStackTrace());
+        }
+
+        System.out.println();
+        llp.printData();
+
+    }
+}
