965581dfcbb507c63a5f0ed4fe4c1f60db59c2c1
abhinav3254 <abhinav3254@gmail.com>
2022-01-18 16:24:56 +0530

    Adding node at the last

--- src/DSA/doublylinkedlist/learnt/L1.java
+++ src/DSA/doublylinkedlist/learnt/L1.java
@@ -16,6 +16,18 @@ public class L1 {
         }
     }
 
+    public void addLast(int data){
+        Node node = new Node(data);
+        if(head == null){
+            head = node;
+            tail = node;
+        } else {
+            node.prev = tail;
+            tail.next = node;
+            tail = node;
+        }
+    }
+
     void displayHead(){
         Node temp = head;
         while (temp!=null){
@@ -47,11 +59,20 @@ public class L1 {
 class A1{
     public static void main(String[] args) {
         L1 l1 = new L1();
-        l1.addFirst(1);
-        l1.addFirst(2);
-        l1.addFirst(3);
-        l1.addFirst(4);
-        l1.addFirst(5);
+//        l1.addFirst(1);
+//        l1.addFirst(2);
+//        l1.addFirst(3);
+//        l1.addFirst(4);
+//        l1.addFirst(5);
+//
+//        l1.displayHead();
+//        l1.displayTail();
+
+        l1.addLast(1);
+        l1.addLast(2);
+        l1.addLast(3);
+        l1.addLast(4);
+        l1.addLast(5);
 
         l1.displayHead();
         l1.displayTail();
