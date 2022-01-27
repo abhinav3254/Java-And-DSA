2a979328f4cc8809cb91aa7a44cdf311a9dbcfba
abhinav3254 <abhinav3254@gmail.com>
2022-01-27 13:26:19 +0530

    Pattern In Recursion

---
+++ src/DSA/recursion/pattern/Pattern1.java
@@ -0,0 +1,25 @@
+package DSA.recursion.pattern;
+
+//  * * * *
+//  * * *
+//  * *
+//  *
+
+public class Pattern1 {
+    public static void main(String[] args) {
+        pattern(4,0);
+    }
+
+    private static void pattern(int row , int column) {
+        if(row == 0){
+            return;
+        }
+        if(column<row){
+            System.out.print("* ");
+            pattern(row,column+1);
+        } else{
+            System.out.println();
+            pattern(row-1,0);
+        }
+    }
+}
