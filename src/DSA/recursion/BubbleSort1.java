cde290758ab67e67bddd1d8ec753f12675e0afa3
abhinav3254 <abhinav3254@gmail.com>
2022-01-29 10:34:40 +0530

    Bubble Sort Recursion

---
+++ src/DSA/recursion/BubbleSort1.java
@@ -0,0 +1,28 @@
+package DSA.recursion;
+
+import java.util.Arrays;
+
+public class BubbleSort1 {
+
+    public static void main(String[] args) {
+        int[] arr = {4,3,2,1};
+        bubble(arr,arr.length-1,0);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void bubble(int[] arr,int r,int c){
+        if(r==0){
+            return;
+        }
+        if(c<r){
+            if(arr[c]>arr[c+1]){
+                int temp = arr[c];
+                arr[c] = arr[c+1];
+                arr[c+1] = temp;
+            }
+            bubble(arr,r,c+1);
+        } else {
+            bubble(arr,r-1,0);
+        }
+    }
+}
