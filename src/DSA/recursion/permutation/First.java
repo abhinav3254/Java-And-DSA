ef4dec7e4e66c56d249d7444eb241712fa304ada
abhinav3254 <abhinav3254@gmail.com>
2022-01-30 18:08:48 +0530

    Permutation of Strings

---
+++ src/DSA/recursion/permutation/First.java
@@ -0,0 +1,20 @@
+package DSA.recursion.permutation;
+
+public class First {
+    private static void permutation(String name , String newName){
+        if(name.isEmpty()){
+            System.out.println(newName);
+            return;
+        }
+        char ch = name.charAt(0);
+        for(int i = 0;i<=newName.length();i++){
+            String first = newName.substring(0,i);
+            String second = newName.substring(i,newName.length());
+            permutation(name.substring(1),first+ch+second);
+        }
+    }
+
+    public static void main(String[] args) {
+        permutation("abc","");
+    }
+}
