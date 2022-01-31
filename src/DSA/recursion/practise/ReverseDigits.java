de3b0a5b1b3841d43e9b01a581fe5eb4311fad97
abhinav3254 <abhinav3254@gmail.com>
2022-01-31 12:26:40 +0530

    is Palindrome or not

--- src/DSA/recursion/practise/ReverseDigits.java
+++ src/DSA/recursion/practise/ReverseDigits.java
@@ -2,8 +2,7 @@ package DSA.recursion.practise;
 
 public class ReverseDigits {
     public static void main(String[] args) {
-        int ans = rev(1234,0);
-        System.out.println(ans);
+        System.out.println(isPalindrome(123821));
     }
     private static int rev(int n,int sum){
         if(n==0){
@@ -11,5 +10,14 @@ public class ReverseDigits {
         }
         sum = sum*10+n%10;
         return rev(n/10,sum);
+    }
+
+    private static boolean isPalindrome(int n){
+        int ans = rev(n,0);
+        if(ans == n){
+            return true;
+        } else{
+            return false;
+        }
     }
 }
