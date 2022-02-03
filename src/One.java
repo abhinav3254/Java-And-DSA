25ee3f9faae82049858ba4349dc54d1eb50a16e8
abhinav3254 <abhinav3254@gmail.com>
2022-02-03 12:24:54 +0530

    How to hit api using java

---
+++ src/One.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class One {
+    public static void main(String[] args) throws IOException {
+        MyGETRequest();
+    }
+    public static void MyGETRequest() throws IOException {
+        URL urlForGetRequest = new URL("https://jsonplaceholder.typicode.com/posts/1");
+        String readLine = null;
+        HttpURLConnection conection = (HttpURLConnection) urlForGetRequest.openConnection();
+        conection.setRequestMethod("GET");
+        //conection.setRequestProperty("userId", "a1bcdef");
+        // set userId it's a sample here
+        int responseCode = conection.getResponseCode();
+
+
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(conection.getInputStream()));
+            StringBuffer response = new StringBuffer();
+            while ((readLine = in .readLine()) != null) {
+                response.append(readLine);
+            } in .close();
+            // print result
+            System.out.println("JSON String Result " + response.toString());
+            //GetAndPost.POSTRequest(response.toString());
+        } else {
+            System.out.println("GET NOT WORKED");
+        }
+
+    }
+}
