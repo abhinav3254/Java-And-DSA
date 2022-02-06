9340bf6e1352aad0f404f132b4fecd9af90d8e1b
abhinav3254 <abhinav3254@gmail.com>
2022-02-06 12:18:10 +0530

    Maze Problem

---
+++ src/DSA/backtracking/Maze1.java
@@ -0,0 +1,16 @@
+package DSA.backtracking;
+
+public class Maze1 {
+    public static void main(String[] args) {
+        System.out.println(maze(3,3));
+    }
+    private static int maze(int row , int column) {
+        if(row == 1 || column == 1) {
+            return 1;
+        }
+        return (maze(row-1,column)+maze(row,column-1));
+//        int left = maze(row-1,column);
+//        int right = maze(row,column-1);
+//        return left+right;
+    }
+}
