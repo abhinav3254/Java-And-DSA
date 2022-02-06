88538c382272a5557906c2b61ce7b674237c1be7
abhinav3254 <abhinav3254@gmail.com>
2022-02-06 22:21:56 +0530

    maze with diagonal path init practise

---
+++ src/DSA/backtracking/maze/MazePathTwo.java
@@ -0,0 +1,22 @@
+package DSA.backtracking.maze;
+
+public class MazePathTwo {
+    public static void main(String[] args) {
+        maze("",3,3);
+    }
+    private static void maze(String newName,int r,int c) {
+        if(r == 1 && c == 1) {
+            System.out.println(newName);
+            return;
+        }
+        if(r>1 && c>1) {
+            maze(newName+'D',r-1,c-1);
+        }
+        if(r>1) {
+            maze(newName+'V',r-1,c);
+        }
+        if(c>1) {
+            maze(newName+"R",r,c-1);
+        }
+    }
+}
