7c0b05d2ff76589b665237172cec142541d2c406
abhinav3254 <abhinav3254@gmail.com>
2022-02-06 13:01:02 +0530

    Maze Problem printing path

---
+++ src/DSA/backtracking/MazePathOne.java
@@ -0,0 +1,19 @@
+package DSA.backtracking;
+
+public class MazePathOne {
+    public static void main(String[] args) {
+        path("",3,3);
+    }
+    private static void path(String newName , int row , int column) {
+        if(row == 1 && column == 1){
+            System.out.println(newName);
+            return;
+        }
+        if(row>1) {
+            path(newName+'D',row-1,column);
+        }
+        if(column>1) {
+            path(newName+'R',row,column-1);
+        }
+    }
+}
