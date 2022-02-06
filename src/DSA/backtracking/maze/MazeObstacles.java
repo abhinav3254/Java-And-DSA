b7532ab4efa9c17276740e903390549b44b93810
abhinav3254 <abhinav3254@gmail.com>
2022-02-06 23:12:42 +0530

    Maze With Obstacles

---
+++ src/DSA/backtracking/maze/MazeObstacles.java
@@ -0,0 +1,27 @@
+package DSA.backtracking.maze;
+
+public class MazeObstacles {
+    public static void main(String[] args) {
+        boolean[][] arr = {
+                {true,true,true},
+                {true,false,true},
+                {true,true,true}
+        };
+        maze("",arr,0,0);
+    }
+    private static void maze(String newName,boolean[][]arr,int r , int c){
+        if(r == arr.length-1 && c == arr[0].length-1){
+            System.out.println(newName);
+            return;
+        }
+        if(arr[r][c] == false){
+            return;
+        }
+        if(r<arr.length-1){
+            maze(newName+"D",arr,r+1,c);
+        }
+        if(c<arr[0].length-1){
+            maze(newName+"R",arr,r,c+1);
+        }
+    }
+}
