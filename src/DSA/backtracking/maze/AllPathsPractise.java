cfc6e69e972a737f5a47078abb4c422434114099
abhinav3254 <abhinav3254@gmail.com>
2022-02-07 12:28:43 +0530

    Backtracking

---
+++ src/DSA/backtracking/maze/AllPathsPractise.java
@@ -0,0 +1,38 @@
+package DSA.backtracking.maze;
+
+public class AllPathsPractise {
+    public static void main(String[] args) {
+        boolean[][] arr = {
+                {true,true,true},
+                {true,true,true},
+                {true,true,true}
+        };
+        allPaths("",arr,0,0);
+    }
+
+    private static void allPaths(String newName , boolean[][] arr,int r, int c) {
+        if(r == arr.length-1 && c == arr[0].length-1) {
+            System.out.println(newName);
+            return;
+        }
+        if(!arr[r][c]){
+            return;
+        }
+        arr[r][c] = false;
+        if(!arr[r][c]) {
+            if(r<arr.length-1) {
+                allPaths(newName+"D", arr, r+1, c);
+            }
+            if(c<arr[0].length-1) {
+                allPaths(newName+"R", arr, r, c+1);
+            }
+            if(r>0) {
+                allPaths(newName+"U", arr, r-1, c);
+            }
+            if(c>0) {
+                allPaths(newName+"D", arr, r, c-1);
+            }
+        }
+        arr[r][c] = true;
+    }
+}
