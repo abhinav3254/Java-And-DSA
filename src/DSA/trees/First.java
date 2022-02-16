a5bf88cf2d500591242271459f020cc3ba7ff92e
abhinav3254 <abhinav3254@gmail.com>
2022-02-16 22:40:19 +0530

    Traversal of Trees

--- src/DSA/trees/First.java
+++ src/DSA/trees/First.java
@@ -23,10 +23,32 @@ public class First {
         return node;
     }
 
-    public static void main(String[] args) {
-        createNode();
+    public static void inOrder(Node root) {
+        if(root == null) {
+            return;
+        }
+        inOrder(root.left);
+        System.out.println(root.data);
+        inOrder(root.right);
     }
 
+    public static void preOrder(Node root) {
+        if(root == null) {
+            return;
+        }
+        System.out.println(root.data);
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+    public static void postOrder(Node root) {
+        if(root == null) {
+            return;
+        }
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.println(root.data);
+
+    }
     static class Node {
         Node right;
         int data;
---
+++ src/DSA/trees/Traversal.java
@@ -0,0 +1,19 @@
+package DSA.trees;
+
+public class Traversal {
+    public static void main(String[] args) {
+        First first = new First();
+
+        First.Node node = first.createNode();
+
+        System.out.println("InOrder:- ");
+        first.inOrder(node);
+
+        System.out.println("Preorder");
+        first.preOrder(node);
+
+        System.out.println("PostOrder");
+        first.postOrder(node);
+
+    }
+}
