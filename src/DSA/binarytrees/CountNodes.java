d2287b6f3c7b43e8e7fc32c3bb52df01f409fb02
abhinav3254 <abhinav3254@gmail.com>
2022-02-17 18:04:53 +0530

    Count Nodes In the Binary Tree

---
+++ src/DSA/binarytrees/CountNodes.java
@@ -0,0 +1,65 @@
+package DSA.binarytrees;
+
+import java.util.Scanner;
+
+public class CountNodes {
+
+    private int count;
+    Scanner sc = new Scanner(System.in);
+    public Node createTree() {
+        Node node;
+        System.out.print("Enter Data:- ");
+        int data = sc.nextInt();
+
+        if(data == -1) {
+            return null;
+        }
+
+        node = new Node(data);
+        count++;
+
+        System.out.println(data+"'s left");
+        node.left = createTree();
+        System.out.println(data+"'s right");
+        node.right = createTree();
+
+        return node;
+    }
+
+    public int countNode () {
+        return count;
+    }
+
+    public void inOrder (Node node) {
+        if(node == null) {
+            return;
+        }
+
+        inOrder(node.left);
+        System.out.print(node.data+" ");
+        inOrder(node.right);
+    }
+
+
+    class Node {
+        Node left;
+        int data;
+        Node right;
+
+        Node (int data) {
+            this.data = data;
+        }
+    }
+}
+
+class Main2 {
+    public static void main(String[] args) {
+        CountNodes countNodes = new CountNodes();
+
+        CountNodes.Node node = countNodes.createTree();
+
+        countNodes.inOrder(node);
+
+        System.out.println("Total Node in the tree is :- "+ countNodes.countNode());
+    }
+}
