607b11d8eb2ad958b1871c1d4fb859e98c619828
abhinav3254 <abhinav3254@gmail.com>
2022-03-18 12:36:46 +0530

    Level Order Travsersal In Binary Search Tree

--- src/DSA/binary_search_trees/BSTOne.java
+++ src/DSA/binary_search_trees/BSTOne.java
@@ -6,16 +6,16 @@ public class BSTOne {
             return createFisrtNode(val);
         }
         if (val < node.data) {
-            insert(node.left, val);
+            node.left = insert(node.left, val);
         } else if (val > node.data) {
-            insert(node.right, val);
+            node.right = insert(node.right, val);
         }
 
         return node;
     }
 
     public Node createFisrtNode(int val) {
-        Node node = new Node(val);
+        Node node = new Node();
         node.left = null;
         node.data = val;
         node.right = null;
@@ -26,41 +26,87 @@ public class BSTOne {
     // Level Order Travsersal
     public void displayLOT(Node node) {
         int h = height(node);
-        for(int i = 1;i<=h;i++) {
-
+        System.out.println();
+        System.out.println("Height of the tree is :- " + h);
+        System.out.println();
+        for (int i = 1; i <= h; i++) {
+            helper(node, i);
         }
     }
 
-    public void helper(Node node) {
-        
+    private void helper(Node node, int height) {
+        // if (node == null) {
+        //     return;
+        // }
+        // if (height == 1) {
+        //     System.out.print(node.data + " ");
+        // } else if (height > 1) {
+        //     helper(node.left, height - 1);
+        //     helper(node.right, height - 1);
+        // }
+        if(node == null) {
+            return;
+        }
+        if(height == 1) {
+            System.out.print(node.data+" ");
+        }
+        else if (height>1) {
+            helper(node.left, height-1);
+            helper(node.right, height-1);
+        }
     }
 
-    public int height (Node node) {
-        if(node == null) {
+    private int height(Node node) {
+        if (node == null) {
             return 0;
         }
         int left = height(node.left);
         int right = height(node.right);
 
-        return Math.max(left, right)+1;
+        return Math.max(left, right) + 1;
     }
 
-    class Node {
-        int data;
-        Node left;
-        Node right;
-
-        Node(int data) {
-            this.data = data;
+    public void display(Node node) {
+        if (node == null) {
+            return;
         }
+        System.out.print(node.data + " ");
+        display(node.left);
+        display(node.right);
     }
+}
 
-    class BSTApp {
-        public static void main(String[] args) {
-            BSTOne bstOne = new BSTOne();
-            Node root = null;
+class Node {
+    int data;
+    Node left;
+    Node right;
+}
 
-            bstOne.insert(root, 8);
-        }
+class BSTApp {
+    public static void main(String[] args) {
+        BSTOne bstOne = new BSTOne();
+        Node root = null;
+
+        root = bstOne.insert(root, 8);
+        root = bstOne.insert(root, 3);
+        root = bstOne.insert(root, 6);
+        root = bstOne.insert(root, 10);
+        root = bstOne.insert(root, 4);
+        root = bstOne.insert(root, 7);
+        root = bstOne.insert(root, 1);
+        root = bstOne.insert(root, 14);
+        root = bstOne.insert(root, 13);
+
+        // bstOne.displayLOT(root);
+
+        System.out.println("Normal travsersal");
+        bstOne.display(root);
+
+        // Level Order Travsersal
+        System.out.println();
+        System.out.println("Level Order Travsersal Starts Here");
+        bstOne.displayLOT(root);
+
+
     }
 }
