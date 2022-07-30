1bda41a1c09371dc0982af10ef48eff05512becb
abhinav3254 <abhinav3254@gmail.com>
2022-07-31 00:58:53 +0530

    change

--- src/questions/EvenOdd.java
+++ src/questions/EvenOdd.java
@@ -1,6 +1,10 @@
 package questions;
 
 public class EvenOdd {
+
+    EvenOdd() {
+        System.out.println("Hello World");
+    }
     public static void main(String[] args) {
         int n = 13;
         if((n&1)==0) System.out.println("Even");
