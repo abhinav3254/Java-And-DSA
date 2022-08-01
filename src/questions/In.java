6ff15f31b00c96e2be5d0c7ae884db86b5e56fd8
abhinav3254 <abhinav3254@gmail.com>
2022-08-01 21:46:54 +0530

    change remove

--- src/questions/In.java
+++ src/questions/In.java
@@ -3,5 +3,14 @@ package questions;
 public class In {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println(check(12));
+    }
+
+    public static boolean check(int n) {
+        if((n&1)==0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
