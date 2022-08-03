ee92588e701d9fa0ab844a0677e6ae70e5514bfd
abhinav3254 <abhinav3254@gmail.com>
2022-08-03 13:09:07 +0530

    Generics Linked List

---
+++ src/DSA/genrics/One.java
@@ -0,0 +1,56 @@
+package DSA.genrics;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class One<T> {
+
+    Node<T> head = null;
+
+    public void addData(T data) {
+        Node<T> node = new Node<T>(data);
+        if(head == null) {
+            head = node;
+        } else {
+            Node<T> temp = head;
+            while (temp.next!=null) {
+                temp = temp.next;
+            }
+            temp.next = node;
+        }
+    }
+
+    public void printData() {
+        Node<T> temp = head;
+        while (temp!=null) {
+            System.out.print(temp.data+" --> ");
+            temp = temp.next;
+        }
+        System.out.println("X");
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = 0;
+        One<String> one = new One<>();
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Enter Data:- ");
+            String data = sc.nextLine();
+            one.addData(data);
+        }
+
+        one.printData();
+    }
+}
+
+class Node<T> {
+    T data;
+    Node next;
+
+    Node(T data) {
+        this.data = data;
+        this.next = null;
+    }
+}
