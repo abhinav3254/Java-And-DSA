2f9d50581347a476e5abb853d80077c7d32033a7
abhinav3254 <abhinav3254@gmail.com>
2022-08-03 13:56:05 +0530

    Binary Tree

--- src/DSA/binarytrees/BinTwo.java
+++ src/DSA/binarytrees/BinTwo.java
@@ -5,12 +5,37 @@ public class BinTwo {
         if(node == null) {
             return new Node1(data);
         } else if(data<node.data){
-            node.left = insert(node,data);
+            node.left = insert(node.left,data);
         } else if(data> node.data){
-            node.right = insert(node, data);
+            node.right = insert(node.right, data);
         }
 
         return node;
+    }
+
+    public void print(Node1 node) {
+        if(node == null) {
+            return;
+        }
+        print(node.left);
+        System.out.print(node.data+" ");
+        print(node.right);
+    }
+
+    public static void main(String[] args) {
+        BinTwo binTwo = new BinTwo();
+        Node1 root = null;
+        root = binTwo.insert(root,8);
+        root = binTwo.insert(root,3);
+        root = binTwo.insert(root,6);
+        root = binTwo.insert(root,10);
+        root = binTwo.insert(root,4);
+        root = binTwo.insert(root,7);
+        root = binTwo.insert(root,1);
+        root = binTwo.insert(root,14);
+        root = binTwo.insert(root,13);
+
+        binTwo.print(root);
     }
 }
 
