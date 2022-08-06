75b8fac9514954ca5b08a8a8a41c6fd5d99dbd42
abhinav3254 <abhinav3254@gmail.com>
2022-08-06 19:02:16 +0530

    changes

--- src/DSA/linkedlist/practise/One.java
+++ src/DSA/linkedlist/practise/One.java
@@ -1,6 +1,39 @@
 package DSA.linkedlist.practise;
 
 public class One {
+
+    Node head = null;
+    Node tail = null;
+    public void insert(int data) {
+        Node node = new Node(data);
+
+        if(head==null & tail==null) {
+            head = node;
+            tail = node;
+        } else {
+            tail.next = node;
+            tail = tail.next;
+        }
+    }
+
+    public void print() {
+        Node temp = head;
+        while (temp!=null) {
+            System.out.print(temp.data+" -->  ");
+            temp = temp.next;
+        }
+        System.out.println("X");
+    }
+
+    public static void main(String[] args) {
+        One one = new One();
+        one.insert(10);
+        one.insert(20);
+        one.insert(30);
+        one.insert(40);
+
+        one.print();
+    }
     class Node {
         int data;
         Node next;
