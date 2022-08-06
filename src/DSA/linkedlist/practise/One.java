f3af994b3db42aa5689d71b0f7edaa294f1a6f23
abhinav3254 <abhinav3254@gmail.com>
2022-08-06 18:57:09 +0530

    changes

---
+++ src/DSA/linkedlist/practise/One.java
@@ -0,0 +1,13 @@
+package DSA.linkedlist.practise;
+
+public class One {
+    class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+}
