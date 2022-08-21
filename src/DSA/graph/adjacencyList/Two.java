b6d5e31350c90dd748b09b2d80eaed79cf6113ad
abhinav3254 <abhinav3254@gmail.com>
2022-08-21 23:19:27 +0530

    [BFS] - implementation

--- src/DSA/graph/adjacencyList/Two.java
+++ src/DSA/graph/adjacencyList/Two.java
@@ -5,10 +5,13 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class Two {
+
+    private final int V;
     private final LinkedList<Integer>[] adj;
     private final Queue<Integer> queue;
 
     public Two(int nodes) {
+        this.V = nodes;
         this.adj = new LinkedList[nodes];
         this.queue = new LinkedList<>();
 
@@ -20,6 +23,26 @@ public class Two {
     public void addEdge(int source , int destination) {
         adj[source].add(destination);
     }
+
+    public void BFS(int n) {
+        boolean nodes[] = new boolean[V];
+        queue.add(n);
+        nodes[n] = true;
+
+        while (!queue.isEmpty()) {
+            n = queue.poll();
+            System.out.print(n+" ");
+
+            for (int i = 0; i < adj[n].size(); i++) {
+                int a = adj[n].get(i);
+                if(!nodes[a]) {
+                    queue.add(a);
+                    nodes[a] = true;
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter number of nodes");
@@ -37,5 +60,8 @@ public class Two {
             System.out.println();
             two.addEdge(source,destination);
         }
+
+        System.out.println("BFS Traversal");
+        two.BFS(0);
     }
 }
