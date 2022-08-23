18dae9b19023a0dd34d7c53e63155208b71d05e5
abhinav3254 <abhinav3254@gmail.com>
2022-08-23 12:07:20 +0530

    [BFS] - implementation

--- src/DSA/graph/adjacencyList/bfs/Two.java
+++ src/DSA/graph/adjacencyList/bfs/Two.java
@@ -21,7 +21,37 @@ public class Two {
     public void addEdge(int s,int d) {
         adj[s].add(d);
     }
-    public static void main(String[] args) {
 
+    //BFS Traversal
+
+    public void BFS (int root) {
+        boolean[] visited = new boolean[V];
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            root = queue.poll();
+            System.out.print(root+" ");
+
+            for (int i = 0; i < adj[root].size(); i++) {
+                int a = adj[root].get(i);
+                if(!visited[a]) {
+                    queue.add(a);
+                    visited[a] = true;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Two two = new Two(6);
+
+        two.addEdge(0,1);
+        two.addEdge(0,2);
+        two.addEdge(1,3);
+        two.addEdge(2,3);
+        two.addEdge(3,4);
+        two.addEdge(3,5);
+
+        two.BFS(0);
     }
 }
