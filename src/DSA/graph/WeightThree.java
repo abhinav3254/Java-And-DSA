3f2d8673b27b7532063fe81abca2b703933dddc0
abhinav3254 <abhinav3254@gmail.com>
2022-08-25 20:48:26 +0530

    [Weighted-Graph implementation] -- 25/08/2022

---
+++ src/DSA/graph/WeightThree.java
@@ -0,0 +1,57 @@
+package DSA.graph;
+
+import java.util.LinkedList;
+
+public class WeightThree {
+    static class Edge {
+        int source;
+        int destination;
+        int weight;
+
+        public Edge (int source , int destination , int weight) {
+            this.source = source;
+            this.destination = destination;
+            this.weight = weight;
+        }
+    }
+
+    static class Graph {
+        final private LinkedList<Edge>[] adj;
+        final private int vertices;
+
+        public Graph (int vertices) {
+            this.vertices = vertices;
+            this.adj = new LinkedList[vertices];
+
+            for (int i = 0; i < vertices; i++) {
+                adj[i] = new LinkedList<>();
+            }
+        }
+
+        public void addEdge (int source , int destination , int weight) {
+            Edge edge = new Edge(source,destination,weight);
+            adj[source].add(edge);
+        }
+
+        public void printGraph () {
+            for (int i = 0; i < vertices; i++) {
+                LinkedList<Edge> list = adj[i];
+
+                for (int j = 0; j < adj[i].size(); j++) {
+                    System.out.println(list.get(j).source+" is connected to "+list.get(j).destination+" and has weight of "+list.get(j).weight);
+                }
+            }
+        }
+
+        public static void main(String[] args) {
+            Graph graph = new Graph(4);
+
+            graph.addEdge(0,1,2);
+            graph.addEdge(0,2,1);
+            graph.addEdge(1,3,5);
+            graph.addEdge(2,3,6);
+
+            graph.printGraph();
+        }
+    }
+}
