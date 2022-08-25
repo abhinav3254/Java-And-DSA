cb46afc495d68ae6de519e5e76dabd474c080147
abhinav3254 <abhinav3254@gmail.com>
2022-08-25 16:34:34 +0530

    [Weighted-Graph implementation] -- 25/08/2022

---
+++ src/DSA/graph/WeightTwo.java
@@ -0,0 +1,50 @@
+package DSA.graph;
+
+import java.util.LinkedList;
+
+public class WeightTwo {
+    static class Edge {
+        int source;
+        int destination;
+        int weight;
+
+        public Edge (int source , int destination , int weight) {
+            this.source = source;
+            this.destination = destination;
+            this.weight = weight;
+        }
+    }
+
+    static class Graph {
+        final private int vertices;
+        final private LinkedList<Edge>[] adj;
+
+        public Graph (int vertices) {
+            this.vertices = vertices;
+            this.adj = new LinkedList[vertices];
+
+            for (int i = 0; i < vertices; i++) {
+                adj[i] = new LinkedList<>();
+            }
+        }
+
+        public void addEdge (int source,int destination , int weight) {
+            Edge edge = new Edge(source,destination,weight);
+            adj[source].add(edge);
+        }
+
+        public void printGraph(){
+            for (int i = 0; i <vertices ; i++) {
+                LinkedList<Edge> list = adj[i];
+                for (int j = 0; j <list.size() ; j++) {
+                    System.out.println("vertex-" + i + " is connected to " +
+                            list.get(j).destination + " with weight " + list.get(j).weight);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
