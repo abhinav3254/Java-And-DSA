dfb732a9cb5ec219c937f2a7d454b6d4903a3979
abhinav3254 <abhinav3254@gmail.com>
2022-10-17 11:04:25 +0530

    gfg

--- src/codechef/START53/OddPairs.java
+++ src/codechef/START53/OddPairs.java
@@ -5,14 +5,18 @@ import java.util.Scanner;
 //https://www.codechef.com/submit/ODDPAIRS?tab=statement
 
 public class OddPairs {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-
-        while (t-->0) {
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc = new Scanner (System.in);
+        int T = sc.nextInt();
+        while(T-->0){
             int n = sc.nextInt();
+            if (n%2==0) {
+                n = (n*n)/2;
+            } else {
+                n = ((n*n)-1)/2;
+            }
 
-            n = n*(n+1)/2;
             System.out.println(n);
         }
     }
