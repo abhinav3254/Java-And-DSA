3998e0985ccfc88262d5ae80a05801645e576f48
abhinav3254 <abhinav3254@gmail.com>
2022-10-26 20:11:46 +0530

    Starters 62 Division 4 (Rated) q1

---
+++ src/codechef/Starters62Division4Rated/MAXDIFFMIN.java
@@ -0,0 +1,17 @@
+package codechef.Starters62Division4Rated;
+
+import java.util.Scanner;
+
+public class MAXDIFFMIN {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-->0) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+
+            System.out.println(c-a);
+        }
+    }
+}
