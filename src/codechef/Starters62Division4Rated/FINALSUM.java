906dbb166cd126b4484b281ea01006b2e93a1130
abhinav3254 <abhinav3254@gmail.com>
2022-10-27 20:37:46 +0530

    Starters 62 Division 4 (Rated) q4

--- src/codechef/Starters62Division4Rated/FINALSUM.java
+++ src/codechef/Starters62Division4Rated/FINALSUM.java
@@ -3,9 +3,51 @@ package codechef.Starters62Division4Rated;
 import java.util.Scanner;
 
 public class FINALSUM {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int t = sc.nextInt();
+//        while (t-->0) {
+//            int n = sc.nextInt();
+//            int q = sc.nextInt();
+//            int[] arr = new int[n];
+//            for (int i = 0; i < n; i++) {
+//                arr[i] = sc.nextInt();
+//            }
+//            int[] qrr = new int[q*2];
+//            for (int i = 0; i < qrr.length; i++) {
+//                qrr[i] = sc.nextInt();
+//            }
+//
+//            for (int j = 0;j<qrr.length;j=j+2) {
+//                int l = qrr[j];
+//                int r = qrr[j+1];
+//                int c = 0;
+//                for (int i = l-1; i < r; i++) {
+//                    if(c%2==0) {
+//                        arr[i] = arr[i]+1;
+//                    } else {
+//                        arr[i] = arr[i]-1;
+//                    }
+//                    c = c+1;
+//                }
+//            }
+//
+//            System.out.println(sum(arr));
+//        }
+//    }
+//
+    public static int sum (int[] arr) {
+        int sum = 0;
+        for (int j : arr) {
+            sum = sum + j;
+        }
+        return sum;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
+
         while (t-->0) {
             int n = sc.nextInt();
             int q = sc.nextInt();
@@ -13,34 +55,16 @@ public class FINALSUM {
             for (int i = 0; i < n; i++) {
                 arr[i] = sc.nextInt();
             }
-            int[] qrr = new int[q*2];
-            for (int i = 0; i < qrr.length; i++) {
-                qrr[i] = sc.nextInt();
-            }
-
-            for (int j = 0;j<qrr.length;j=j+2) {
-                int l = qrr[j];
-                int r = qrr[j+1];
-                int c = 0;
-                for (int i = l-1; i < r; i++) {
-                    if(c%2==0) {
-                        arr[i] = arr[i]+1;
-                    } else {
-                        arr[i] = arr[i]-1;
-                    }
-                    c = c+1;
+            int c = 0;
+            for (int i = 0; i < q; i++) {
+                int l = sc.nextInt();
+                int r = sc.nextInt();
+                int d = r-l;
+                if (d%2==0) {
+                    c++;
                 }
             }
-
-            System.out.println(sum(arr));
+            System.out.println(sum(arr)+c);
         }
-    }
-
-    public static int sum (int[] arr) {
-        int sum = 0;
-        for (int j : arr) {
-            sum = sum + j;
-        }
-        return sum;
     }
 }
