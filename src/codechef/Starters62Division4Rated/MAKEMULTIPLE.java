cb5418c9cfeda14629b5cf1fa4844bc705f3844a
abhinav3254 <abhinav3254@gmail.com>
2022-10-27 21:11:11 +0530

    Starters 62 Division 4 (Rated) q5

--- src/codechef/Starters62Division4Rated/MAKEMULTIPLE.java
+++ src/codechef/Starters62Division4Rated/MAKEMULTIPLE.java
@@ -3,26 +3,42 @@ package codechef.Starters62Division4Rated;
 import java.util.Scanner;
 
 public class MAKEMULTIPLE {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while (t-->0) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            int c = 0;
-            for (int i = 0;i<=a;i++) {
-                a = a+i;
-                b = b+i;
-
-                if (b%a==0) {
-                    c++;
-                    break;
-                }
-            }
-            if (c!=0) {
-                System.out.println("YES");
-            } else
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int t = sc.nextInt();
+//        while (t-->0) {
+//            int a = sc.nextInt();
+//            int b = sc.nextInt();
+//            int c = 0;
+//            for (int i = 0;i<=a;i++) {
+//                a = a+i;
+//                b = b+i;
+//
+//                if (b%a==0) {
+//                    c++;
+//                    break;
+//                }
+//            }
+//            if (c!=0) {
+//                System.out.println("YES");
+//            } else
+//                System.out.println("NO");
+//        }
+//    }
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int t = sc.nextInt();
+    while (t-->0) {
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if (a == b)
+            System.out.println("YES");
+        else {
+            int d = b-a;
+            if (a>d) {
                 System.out.println("NO");
+            } else System.out.println("YES");
         }
     }
+}
 }
