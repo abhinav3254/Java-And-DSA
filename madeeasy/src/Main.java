8c72310068c4079d748340c7cb715e46f0a608b2
Abhinav Jha <97444941+abhinav3254@users.noreply.github.com>
2022-10-29 21:59:28 +0530

    Update Main.java

--- madeeasy/src/Main.java
+++ madeeasy/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 System.out.println("Hello made easy");
+System.out.println("Hello made easy 29 oct 2022");
     }
 }
