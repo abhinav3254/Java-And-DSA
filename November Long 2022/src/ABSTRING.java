2e914f99ce7e2ddd52d95422bbd57381ca628a57
abhinav3254 <abhinav3254@gmail.com>
2022-11-12 20:46:05 +0530

    NOV Long challenge

---
+++ November Long 2022/src/ABSTRING.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ABSTRING {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-->0) {
+            int n = sc.nextInt();
+            String word = sc.nextLine();
+        }
+    }
+}
