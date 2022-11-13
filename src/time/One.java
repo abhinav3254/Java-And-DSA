04528c79e8e4ee6972627e04d9bd116f7ea21c53
abhinav3254 <abhinav3254@gmail.com>
2022-11-13 17:57:49 +0530

    canges

--- src/time/One.java
+++ src/time/One.java
@@ -4,8 +4,15 @@ public class One {
     public static void main(String[] args) throws InterruptedException {
         long start = System.nanoTime();
         // ------ program
-        Thread.sleep(3000);
+//        Thread.sleep(3000);
         // ---- program
+
+        for (int i = 0; i < 100; i++) {ch
+//            System.out.print("H");
+            for (int j = 0; j < 100; j++) {
+                
+            }
+        }
 
         long duration = (System.nanoTime() - start) / 1000000;
         System.out.println(duration+" ms");
