fae8b4221a9c4d6396cd42fed275dc9f46a5e67a
abhinav3254 <abhinav3254@gmail.com>
2023-06-07 22:24:34 +0530

    question-2

--- out/production/Train/rough/Three.java
+++ out/production/Train/rough/Three.java
@@ -2,7 +2,8 @@ package out.production.Train.rough;
 
 public class Three {
     public static void main(String[] args) {
-        questionOne(10, 10);
+        // questionOne(10, 10);
+        questionTwo(5);
     }
 
     // question-1
@@ -13,6 +14,17 @@ public class Three {
                 System.out.print("- ");
             }
             System.out.println();
+        }
+    }
+
+    // question - 2
+
+    public static void questionTwo(int r) {
+        for (int i = 1; i < r; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" ");
+            }
+            System.out.println("* ");
         }
     }
 
