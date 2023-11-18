832e92797e4af4bcdda76da3101f0a55d47e20e4
abhinav3254 <abhinav3254@gmail.com>
2023-11-18 22:55:11 +0530

    making pretty display

--- graph/src/main/java/org/example/tree/BinaryTree.java
+++ graph/src/main/java/org/example/tree/BinaryTree.java
@@ -68,4 +68,29 @@ public class BinaryTree {
     }
 
 
+    public void prettyDisplay() {
+        prettyDisplay(root,0);
+    }
+
+
+    private void prettyDisplay(Node node, int level) {
+        if (node == null) return;
+
+        prettyDisplay(node.right,level+1);
+        if (level!=0) {
+
+            for (int i = 0; i < level - 1; i++) {
+                System.out.print("|\t\t");
+            }
+            System.out.println("|------->"+node.value);
+        } else {
+            System.out.println(node.value);
+        }
+
+        prettyDisplay(node.left,level+1);
+
+
+    }
+
+
 }
