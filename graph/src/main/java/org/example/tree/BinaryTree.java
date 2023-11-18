d4857e03d0479d54bab73ba00d86f3356ba56b1d
abhinav3254 <abhinav3254@gmail.com>
2023-11-18 22:41:19 +0530

    implemented

--- graph/src/main/java/org/example/tree/BinaryTree.java
+++ graph/src/main/java/org/example/tree/BinaryTree.java
@@ -8,7 +8,7 @@ public class BinaryTree {
 
 
     private static class Node {
-        private int value;
+        private final int value;
         private Node left;
         private Node right;
 
@@ -33,8 +33,39 @@ public class BinaryTree {
         populate(scanner,root);
     }
 
-    private void populate(Scanner scanner,Node root) {
+    private void populate(Scanner scanner,Node node) {
+        System.out.println("Do you want insert left of "+node.value);
+        boolean left = scanner.nextBoolean();
+        if (left) {
+            System.out.println("Enter the value of left of "+node.value);
+            int value = scanner.nextInt();
+            node.left = new Node(value);
+            populate(scanner,node.left);
+        }
 
+        System.out.println("Do you want insert right of "+node.value);
+        boolean right = scanner.nextBoolean();
+        if (right) {
+            System.out.println("Enter the value of right of "+node.value);
+            int value = scanner.nextInt();
+            node.right = new Node(value);
+            populate(scanner,node.right);
+        }
     }
+
+
+    public void display() {
+        display(root,"");
+    }
+
+    private void display(Node node,String indent) {
+        if (node==null) {
+            return;
+        }
+        System.out.println(indent+node.value);
+        display(node.left,"\t");
+        display(node.right,"\t");
+    }
+
 
 }
